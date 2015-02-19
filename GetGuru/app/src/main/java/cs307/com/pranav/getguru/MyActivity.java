ea915c3c76f61d600a3a1d2aa126ae71120ba754
Merctron <smartomot@hotmail.com>
2015-02-19 16:09:12 -0500

    Added AsnycTask and OnClickListener

--- GetGuru/app/src/main/java/cs307/com/pranav/getguru/MyActivity.java
+++ GetGuru/app/src/main/java/cs307/com/pranav/getguru/MyActivity.java
@@ -1,12 +1,32 @@
 package cs307.com.pranav.getguru;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
 
-public class MyActivity extends ActionBarActivity {
+public class MyActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +52,71 @@ public class MyActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    protected String addLocationToUrl(String url){
+        if(!url.endsWith("?"))
+            url += "?";
+
+        List<BasicNameValuePair> params = new LinkedList<BasicNameValuePair>();
+
+
+        params.add(new BasicNameValuePair("message", "hello"));
+        params.add(new BasicNameValuePair("me", "1"));
+        params.add(new BasicNameValuePair("you", "2"));
+
+        String paramString = URLEncodedUtils.format(params, "utf-8");
+
+        url += paramString;
+        return url;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+        }
+    }
+
+    private class NetworkTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object... params) {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(addLocationToUrl("http://ec2-54-91-89-2.compute-1.amazonaws.com:8080/box"));
+
+            //making GET request.
+            InputStream inputStream = null;
+            String responsestr = "";
+            try {
+                HttpResponse response = httpClient.execute(httpGet);
+                // write response to log
+                inputStream = response.getEntity().getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null)
+                {
+                    sb.append(line + "\n");
+                }
+
+                responsestr = sb.toString();
+                Log.d("Http Get Response:", responsestr);
+                JSONObject json = new JSONObject(responsestr);
+
+
+            } catch (ClientProtocolException e) {
+                // Log exception
+                e.printStackTrace();
+            } catch (IOException e) {
+                // Log exception
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
