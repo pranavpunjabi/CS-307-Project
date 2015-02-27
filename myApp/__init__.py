dc874330897326f8eb9b13d373463e46cac103b7
Swagath Venkataramani <swagathvenkataramani@Swagaths-MacBook-Pro.local>
2015-02-27 01:28:02 -0500

    updated ifTut

---
+++ myApp/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+myApp = Flask(__name__)
+myApp.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://balasubv:balasubv@localhost/GetGuru'
+myApp.secret_key = 'development key'
+from models import db
+
+db.init_app(myApp)
+
+from myApp import views
+
