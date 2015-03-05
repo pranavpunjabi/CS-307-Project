ca9d7545af36501e0e0209363cbd3c392f87d570
Merctron <smartomot@hotmail.com>
2015-03-04 20:28:56 -0500

    Switchedto one master tab activity

---
+++ GetGuru/app/src/main/java/cs307/com/pranav/getguru/MasterTabHost.java
@@ -0,0 +1,93 @@
+package cs307.com.pranav.getguru;
+
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.widget.TabHost;
+
+
+public class MasterTabHost extends FragmentActivity {
+
+    TabHost tHost;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_host_layout);
+
+        tHost = (TabHost) findViewById(android.R.id.tabhost);
+        tHost.setup();
+
+        /** Defining Tab Change Listener event. This is invoked when tab is changed */
+        TabHost.OnTabChangeListener tabChangeListener = new TabHost.OnTabChangeListener() {
+
+            @Override
+            public void onTabChanged(String tabId) {
+                android.support.v4.app.FragmentManager fm =   getSupportFragmentManager();
+                AndroidFragment androidFragment = (AndroidFragment) fm.findFragmentByTag("android");
+                StudentProfileFragment profileFragment = (StudentProfileFragment) fm.findFragmentByTag("profile");
+                SearchFragment searchFragment = (SearchFragment) fm.findFragmentByTag("search");
+                android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
+
+                /** Detaches the androidfragment if exists */
+                if(androidFragment!=null)
+                    ft.detach(androidFragment);
+                if(profileFragment!=null)
+                    ft.detach(profileFragment);
+                if(searchFragment!=null)
+                    ft.detach(searchFragment);
+
+                /** If current tab is android */
+                if(tabId.equalsIgnoreCase("android")){
+
+                    if(androidFragment==null){
+                        /** Create AndroidFragment and adding to fragmenttransaction */
+                        ft.add(R.id.realtabcontent,new AndroidFragment(), "android");
+                    }else{
+                        /** Bring to the front, if already exists in the fragmenttransaction */
+                        ft.attach(androidFragment);
+                    }
+
+                }
+                if(tabId.equalsIgnoreCase("profile")){
+
+                    if(profileFragment==null){
+                        /** Create AndroidFragment and adding to fragmenttransaction */
+                        ft.add(R.id.realtabcontent,new StudentProfileFragment(), "profile");
+                    }else{
+                        /** Bring to the front, if already exists in the fragmenttransaction */
+                        ft.attach(profileFragment);
+                    }
+
+                }
+                ft.commit();
+            }
+        };
+
+        /** Setting tabchangelistener for the tab */
+        tHost.setOnTabChangedListener(tabChangeListener);
+
+        /** Defining tab builder for Profile tab */
+        TabHost.TabSpec tSpecProfile = tHost.newTabSpec("profile");
+        tSpecProfile.setIndicator("Profile");
+        tSpecProfile.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecProfile);
+
+        /** Defining tab builder for Search tab */
+        TabHost.TabSpec tSpecSearch = tHost.newTabSpec("search");
+        tSpecSearch.setIndicator("Search");
+        tSpecSearch.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecSearch);
+
+        /** Defining tab builder for Android (test) tab */
+        TabHost.TabSpec tSpecAnd = tHost.newTabSpec("android");
+        tSpecAnd.setIndicator("Android");
+        tSpecAnd.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecAnd);
+
+    }
+}
---
+++ GetGuru/app/src/main/java/cs307/com/pranav/getguru/StudentProfileFragment.java
@@ -0,0 +1,58 @@
+package cs307.com.pranav.getguru;
+
+/**
+ * Created by DiGiT_WiZARD on 2/26/15.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class StudentProfileFragment extends Fragment implements OnClickListener {
+
+
+    Button toggle, edit;
+    View masterView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View masterView = inflater.inflate(R.layout.profile_student, container, false);
+
+        toggle = (Button) masterView.findViewById(R.id.toggle);
+        edit = (Button) masterView.findViewById(R.id.editInfo);
+
+        toggle.setOnClickListener(this);
+        edit.setOnClickListener(this);
+
+        return masterView;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.editInfo:
+                Intent i = new Intent(this.getActivity(), Edit.class);
+                startActivity(i);
+                break;
+            case R.id.toggle:
+                Intent j = new Intent(this.getActivity(), TutorProfile.class);
+                startActivity(j);
+                break;
+        }
+    }
+}
+
