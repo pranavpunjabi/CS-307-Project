dad45631a24be779284bea5f3b216326606c2128
Merctron <smartomot@hotmail.com>
2015-03-04 19:26:51 -0500

    Clean code

--- GetGuru/app/src/main/java/cs307/com/pranav/getguru/TutorProfile.java
+++ GetGuru/app/src/main/java/cs307/com/pranav/getguru/TutorProfile.java
@@ -1,39 +1,92 @@
 package cs307.com.pranav.getguru;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.widget.TabHost;
 
 
-public class TutorProfile extends ActionBarActivity {
+public class TutorProfile extends FragmentActivity {
+
+    TabHost tHost;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutor_profile);
-    }
+        setContentView(R.layout.tab_host_layout);
 
+        tHost = (TabHost) findViewById(android.R.id.tabhost);
+        tHost.setup();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tutor_profile, menu);
-        return true;
-    }
+        /** Defining Tab Change Listener event. This is invoked when tab is changed */
+        TabHost.OnTabChangeListener tabChangeListener = new TabHost.OnTabChangeListener() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            @Override
+            public void onTabChanged(String tabId) {
+                android.support.v4.app.FragmentManager fm =   getSupportFragmentManager();
+                AndroidFragment androidFragment = (AndroidFragment) fm.findFragmentByTag("android");
+                Profile profileFragment = (Profile) fm.findFragmentByTag("profile");
+                Search searchFragment = (Search) fm.findFragmentByTag("search");
+                android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                /** Detaches the androidfragment if exists */
+                if(androidFragment!=null)
+                    ft.detach(androidFragment);
+                if(profileFragment!=null)
+                    ft.detach(profileFragment);
+                if(searchFragment!=null)
+                    ft.detach(searchFragment);
 
-        return super.onOptionsItemSelected(item);
+                /** If current tab is android */
+                if(tabId.equalsIgnoreCase("android")){
+
+                    if(androidFragment==null){
+                        /** Create AndroidFragment and adding to fragmenttransaction */
+                        ft.add(R.id.realtabcontent,new AndroidFragment(), "android");
+                    }else{
+                        /** Bring to the front, if already exists in the fragmenttransaction */
+                        ft.attach(androidFragment);
+                    }
+
+                }
+                if(tabId.equalsIgnoreCase("profile")){
+
+                    if(profileFragment==null){
+                        /** Create AndroidFragment and adding to fragmenttransaction */
+                        ft.add(R.id.realtabcontent,new Profile(), "profile");
+                    }else{
+                        /** Bring to the front, if already exists in the fragmenttransaction */
+                        ft.attach(profileFragment);
+                    }
+
+                }
+                ft.commit();
+            }
+        };
+
+        /** Setting tabchangelistener for the tab */
+        tHost.setOnTabChangedListener(tabChangeListener);
+
+        /** Defining tab builder for Profile tab */
+        TabHost.TabSpec tSpecProfile = tHost.newTabSpec("profile");
+        tSpecProfile.setIndicator("Profile");
+        tSpecProfile.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecProfile);
+
+        /** Defining tab builder for Search tab */
+        TabHost.TabSpec tSpecSearch = tHost.newTabSpec("search");
+        tSpecSearch.setIndicator("Search");
+        tSpecSearch.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecSearch);
+
+        /** Defining tab builder for Android (test) tab */
+        TabHost.TabSpec tSpecAnd = tHost.newTabSpec("android");
+        tSpecAnd.setIndicator("Android");
+        tSpecAnd.setContent(new TabContent(getBaseContext()));
+        tHost.addTab(tSpecAnd);
+
     }
 }
