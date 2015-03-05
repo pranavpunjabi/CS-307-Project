3bc850ba2cde2651d646fe25be0e51599d4c92cd
Merctron <smartomot@hotmail.com>
2015-03-04 21:48:30 -0500

    Smooth transitions executed

--- GetGuru/app/src/main/java/cs307/com/pranav/getguru/TutorProfileFragment.java
+++ GetGuru/app/src/main/java/cs307/com/pranav/getguru/TutorProfileFragment.java
@@ -1,5 +1,6 @@
 package cs307.com.pranav.getguru;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -22,11 +23,11 @@ public class TutorProfileFragment extends Fragment implements OnClickListener {
         //return super.onCreateView(inflater, container, savedInstanceState);
         masterView = inflater.inflate(R.layout.profile_tutor, container, false);
 
-        //toggle = (Button) masterView.findViewById(R.id.toggle);
-        //edit = (Button) masterView.findViewById(R.id.editInfo);
+        toggle = (Button) masterView.findViewById(R.id.toggle2);
+        edit = (Button) masterView.findViewById(R.id.editInfo2);
 
-        //toggle.setOnClickListener(this);
-        //edit.setOnClickListener(this);
+        toggle.setOnClickListener(this);
+        edit.setOnClickListener(this);
 
         return masterView;
 
@@ -40,7 +41,14 @@ public class TutorProfileFragment extends Fragment implements OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.editInfo2:
+                Intent i = new Intent(this.getActivity(), Edit.class);
+                startActivity(i);
+                break;
+            case R.id.toggle2:
+                Intent j = new Intent(this.getActivity(), StudentTabHost.class);
+                startActivity(j);
+                break;
         }
     }
 }
