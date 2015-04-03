3e0532d9fdb4265d1bd24325ad625c5abcb24bb6
pranavpunjabi <pranavpunjabi@gmail.com>
2015-04-03 17:26:46 -0400

    List View sample

---
+++ ListView/app/src/main/java/com/example/pranav/listview/Model.java
@@ -0,0 +1,32 @@
+package com.example.pranav.listview;
+
+/**
+ * Created by Pranav on 4/3/2015.
+ */
+public class Model {
+
+    private String name;
+    private boolean selected;
+
+    public Model(String name) {
+        this.name = name;
+        selected = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+}
